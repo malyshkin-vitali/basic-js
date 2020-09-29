11a10a85c6560878d52599378f06b2a3964a99d7
Vitali Malyshkin <vitalyam13@gmail.com>
2020-09-29 21:33:32 +0300

    fix: correct count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =
+    function countCats(backyard) {
+        if (backyard.length === 0) return 0;
+        let array = backyard[0];
+        for (let i = 1; i < backyard.length; i++) {
+            array = array.concat(backyard[i]);
+        }
+        let count = 0;
+        for (let a = 0; a < array.length; a++) {
+            if (array[a] === '^^') count++;
+        }
+        return count;
+    }
+
+// console.log(countCats([
+//     [undefined, 2],
+//     [3,3,6]
+// ]));
